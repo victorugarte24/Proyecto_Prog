5ff81cf2f2f335ff56cc2896faf9030ccc2e9807
Victor <Victor@DESKTOP-AAJDOL9>
2019-11-20 15:54:25 +0100

    commit

---
+++ proyecto_programacion_III/src/bd/BD_Test.java
@@ -0,0 +1,20 @@
+package bd;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import org.junit.Test;
+
+import usuario.Usuario;
+
+public class BD_Test {
+	
+		Usuario u1 = new Usuario("sergio", "1234", null, null, 0, null, null, null, 0, 0);
+		Usuario u2 = new Usuario("victor", "4321", null, null, 0, null, null, null, 0, 0);
+	
+	
+}
