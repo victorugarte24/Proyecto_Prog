17ff30ef1af2c4ead1d69188af50f66bf46bd689
manu2610 <manuel.delhoyo@opendeusto.es>
2019-11-20 12:49:00 +0100

    Herencias

---
+++ proyecto_programacion_III/src/electrodomesticos/Electrodomesticos.java
@@ -0,0 +1,45 @@
+package electrodomesticos;
+
+import articulo.Articulo;
+
+public class Electrodomesticos extends Articulo{
+	
+	private String tipo;
+	private int peso;
+	private double tamaño;
+	
+	public Electrodomesticos(String nombre, String codArt, String descripcion, double importe, String marca,
+			String tipo, int peso, double tamaño) {
+		super(nombre, codArt, descripcion, importe, marca);
+		this.tipo = tipo;
+		this.peso = peso;
+		this.tamaño = tamaño;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public double getTamaño() {
+		return tamaño;
+	}
+
+	public void setTamaño(double tamaño) {
+		this.tamaño = tamaño;
+	}
+	
+	
+
+}
