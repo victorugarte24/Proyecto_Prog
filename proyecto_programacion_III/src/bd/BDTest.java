556bd1fe694cc1d7c62104e0837e9addeea4a67a
Victor <Victor@DESKTOP-AAJDOL9>
2020-01-11 17:06:12 +0100

    commit

--- proyecto_programacion_III/src/bd/BDTest.java
+++ proyecto_programacion_III/src/bd/BDTest.java
@@ -13,5 +13,24 @@ import usuario.Usuario;
 
 public class BDTest {
 	
+	private Connection con;
+	private Statement st;
+	
+	@Test
+	public void test() {
+		con = BD.initBD("data/Database");
+		st = BD.usarCrearTablasBD(con);
+		Usuario u = new Usuario("b@gmail.com", "4321");
+		BD.usuariosInsert(st, u);
+		
+		assertEquals("4321", BD.usuariosSelect2(st, "b@gmail.com")); //devuelve la contraseña esperada de la BD
+		
+		assertTrue(BD.usuarioExiste(st, "b@gmail.com")); //El usuario insertado existe en la BD
+
+	}
+
+	//assertNotNull(st);
+	//assertEquals("4321", BD.usuariosSelect(st, "victor"));
+		
 	
 }
